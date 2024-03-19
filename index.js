f8b7df137c52b02685494d3bf91c592d11046762
John Buz <johnbuz@MBP-John.IGD_Rostelecom>
2024-03-19 12:32:30 +0500

    update readme.md

---
+++ index.js
@@ -0,0 +1,12 @@
+// index.js
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App'; // Импортируем компонент App из файла App.js
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
