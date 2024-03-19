047f5182d7f1ab490a228472756acea20ffb7a1d
John Buz <johnbuz@MBP-John.IGD_Rostelecom>
2024-03-19 12:01:37 +0500

    added test command

---
+++ math.test.js
@@ -0,0 +1,19 @@
+// math.test.js
+
+const { add, subtract } = require('./math');
+
+test('adds 1 + 2 to equal 3', () => {
+  expect(add(1, 2)).toBe(3);
+});
+
+test('adds 5 + 7 to equal 12', () => {
+  expect(add(5, 7)).toBe(12);
+});
+
+test('subtracts 5 - 2 to equal 3', () => {
+  expect(subtract(5, 2)).toBe(3);
+});
+
+test('subtracts 10 - 7 to equal 3', () => {
+  expect(subtract(10, 7)).toBe(3);
+});
